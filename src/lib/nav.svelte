c0655dd6c1f715fb185643551a231d1dd274602b
erixweb <erikmorenodev@gmail.com>
2023-10-26 20:07:40 +0200

    Refactor

---
+++ src/lib/nav.svelte
@@ -0,0 +1,9 @@
+<nav class="font-sans text-[20px] fixed top-0 left-0 w-full z-50">
+	<div class="w-full h-full relative py-[10px]">
+		<div class="w-full h-full absolute top-0 left-0 z-0 bg-slate-300/50"/>
+		<div class="m-auto p-[10px] w-full max-w-[1050px] flex gap-[20px] z-20 relative">
+			<a href="/">Inici</a>
+			<a href="/#llocs">Llocs</a>
+		</div>
+	</div>
+</nav>
