88a5823f0db13c3aefa9e3aa4eede5a795c425c2
erixweb <erikmorenodev@gmail.com>
2023-10-26 18:06:55 +0200

    Initial commit

---
+++ src/lib/card.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    export let title: string
+    export let description: string
+    export let image: string
+
+</script>
+<a href="/" class="max-w-[450px] m-auto active:scale-105 transition-all duration-250">
+    <div>
+        <img src={image} alt={`Imatge de ${title}`} class="rounded-[15px]">
+    </div>
+    <div class="p-[1em]">
+        <h2 class="font-bold text-[20px]">
+            {title}
+        </h2>
+        <p>
+            {description}
+        </p>
+    </div>
+</a>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,23 @@
+<script>
+	import Card from "$lib/card.svelte"
+	import "../app.css"
+</script>
+
+<svelte:head>
+	<title>tourxcat</title>
+</svelte:head>
+<main class="font-sans m-auto w-full max-w-[1050px]">
+	<h1>Hello</h1>
+	<section class="my-[20px] grid grid-cols-2 gap-[20px] w-full">
+		<Card
+			title="Vacances a Malgrat de Mar, costa Brava"
+			description="Recomenable pels que volen pasar unes vancances a la costa Brava sense gastar molts diners."
+			image={`https://cdn.discordapp.com/attachments/1152239048220475516/1167127397859069952/malgrat.webp`}
+		/>
+		<Card
+			title="Vacances a Malgrat de Mar, costa Brava"
+			description="Recomenable pels que volen pasar unes vancances a la costa Brava sense gastar molts diners."
+			image={`https://cdn.discordapp.com/attachments/1152239048220475516/1167127397859069952/malgrat.webp`}
+		/>
+	</section>
+</main>
