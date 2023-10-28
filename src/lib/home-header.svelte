7688ad7e4bbb1034c2fb36b9243f08334baeda16
erixweb <erikmorenodev@gmail.com>
2023-10-28 18:08:26 +0200

    Add hover effect

--- src/lib/home-header.svelte
+++ src/lib/home-header.svelte
@@ -1,6 +1,6 @@
 <header
 	class="flex justify-center items-center font-sans m-auto w-full max-w-[1050px] min-h-[100vh] relative mb-[40px] px-[20px]"
 >
-	<h1 class="text-[50px] font-bold">No et perdis en les vacances</h1>
+	<h1 class="text-[50px] font-bold lg:hover:scale-105 cursor-default duration-[200ms]">No et perdis en les vacances</h1>
 	<a href="#llocs" id="finger-down" class="absolute bottom-10 text-[40px]"> 👇 </a>
 </header>
