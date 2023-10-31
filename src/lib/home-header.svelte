5a5716467e524c105bfaf652a9e9e89c3881b6bb
erixweb <erikmorenodev@gmail.com>
2023-10-31 17:47:02 +0100

    Add description

--- src/lib/home-header.svelte
+++ src/lib/home-header.svelte
@@ -1,6 +1,11 @@
 <header
 	class="flex justify-center items-center font-sans m-auto w-full max-w-[1050px] min-h-[100vh] relative mb-[40px] px-[20px]"
 >
-	<h1 class="text-[50px] font-bold lg:hover:scale-105 cursor-default duration-[200ms]">No et perdis en les vacances</h1>
+	<div>
+		<h1 class="text-[50px] font-bold lg:hover:scale-105 cursor-default duration-[200ms]">
+			No et perdis en les vacances
+		</h1>
+		<p class="text-center text-gray-600 mt-[20px]">Si ets dels que es perden quan van de vacances, això s'ha acabat.</p>
+	</div>
 	<a href="#llocs" id="finger-down" class="absolute bottom-10 text-[40px]"> 👇 </a>
 </header>
