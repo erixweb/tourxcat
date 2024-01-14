54c28e0a9dfd7b4259f0d52275964ff2111320a5
erixweb <erikmorenodev@gmail.com>
2024-01-14 17:07:13 +0100

    Add background image

--- src/lib/home-header.svelte
+++ src/lib/home-header.svelte
@@ -1,11 +1,13 @@
+
 <header
-	class="flex justify-center items-center font-sans m-auto w-full max-w-[1050px] min-h-[100vh] relative mb-[40px] px-[20px]"
+	class="flex justify-center items-center font-sans m-auto w-full min-h-[100vh] relative mb-[40px] px-[20px] bg-slate-800"
 >
-	<div>
-		<h1 class="text-[50px] font-bold lg:hover:scale-105 cursor-default duration-[200ms]">
+	<img src="https://www.guiarepsol.com/content/dam/repsol-guia/contenidos-imagenes/viajar/vamos-de-excursion/planes-vic-barcelona/gr-cms-media-featured_images-none-0f0590eb-7744-485e-8103-474156e8b86e-00-casino_vic84.jpg" alt="" class="absolute top-0 left-0 z-[1] w-full object-cover h-full opacity-60">
+	<div class="z-[10] max-w-[1050px]">
+		<h1 class="text-[50px] font-bold lg:hover:scale-105 cursor-default duration-[200ms] text-white">
 			No et perdis en les vacances
 		</h1>
-		<p class="text-center text-gray-600 mt-[20px]">Si ets dels que es perden quan van de vacances, això s'ha acabat.</p>
+		<p class="text-center text-gray-200 mt-[20px]">Si ets dels que es perden quan van de vacances, això s'ha acabat.</p>
 	</div>
-	<a href="#llocs" id="finger-down" class="absolute bottom-10 text-[40px]"> 👇 </a>
+	<a href="#llocs" id="finger-down" class="absolute bottom-10 text-[40px] z-[10]"> 👇 </a>
 </header>
